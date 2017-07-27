70a4a8d1d1c63552d8e4885e0e60e140d83ac1fb
n221199515 <hong_ronald@hotmail.com>
2017-07-27 00:47:44 -0500

    CSS
    remove <a>git</a>/add git fa-icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,7 @@ class Home extends Component {
     this.state={
       email: "hong_ronald@hotmail.com",
       phone: "(832)276-7978",
-      Location: "Houston, Tx",
-      Github: "https://github.com/ronaldhong"
+      Location: "Houston, Tx"
     }
   }
   render() {
@@ -20,11 +19,11 @@ class Home extends Component {
           <div>{this.state.email}</div>
           <div>{this.state.phone}</div>
           <div>{this.state.Location}</div>
-          <div><a href="https://github.com/ronaldhong">My Github</a></div>
         </div>
         <div className="FontAwesome">
           <a href="https://www.facebook.com/ronaldshunhong"><FontAwesome id="fb-icon" className='super-crazy-colors' name='facebook-square' size='2x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/></a>
           <a href="https://www.instagram.com/ron_hong919"><FontAwesome id="instagram-icon" className='super-crazy-colors' name='instagram' size='2x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/></a>
+          <a href="https://github.com/ronaldhong"><FontAwesome id="github-icon" className='super-crazy-colors' name='github' size='2x' style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/></a>
         </div>
         <div className="images">
         </div>
